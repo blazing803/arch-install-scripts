7927fa6a6f5ed09d67d8c3fa7106e2c3ee97c4b0
blazing803 <66576419+blazing803@users.noreply.github.com>
2024-03-09 20:48:44 -0500

    Update arch-install-sata-x64.sh

--- arch-install-sata-x64.sh
+++ arch-install-sata-x64.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 echo -e "d\n1\nd\n2\nd\n3\nw" | fdisk /dev/sda
 echo -e "n\n\n\n+100M\nn\n\n\n+4G\nn\n\n\n\nw" | fdisk /dev/sda
-# Format partitions
 mkfs.fat -F32 /dev/sda1
 mkswap /dev/sda2
 mkfs.ext4 /dev/sda3
 mount /dev/sda3 /mnt
 mkdir -p /mnt/boot/efi
-mount /dev/sda1 /mnt/boot/efi
-swapon /dev/sda2
-pacstrap /mnt base linux linux-firmware sof-firmware base-devel grub efibootmgr nano networkmanager git mesa nvidia ntp dhcpcd networkmanager network-manager-applet alacritty xorg i3 lightdm lightdm-gtk-greeter
+mount /dev/nvme0n1p1 /mnt/boot/efi
+swapon /dev/nvme0n1p2
+pacstrap /mnt base linux linux-firmware sof-firmware base-devel grub efibootmgr nano networkmanager git mesa nvidia ntp dhcpcd  xorg  i3 networkmanager network-manager-applet alacritty lightdm lightdm-gtk-greeter 
 genfstab -U /mnt >> /mnt/etc/fstab
 arch-chroot /mnt <<EOF
 ln -sf /usr/share/zoneinfo/America/New_York /etc/localtime
@@ -30,5 +29,5 @@ systemctl start ntpd.service
 timedatectl set-ntp true
 systemctl enable NetworkManager
 systemctl enable dhcpcd
-systemctl enable lightdm 
+systemctl enable lightdm
 EOF
